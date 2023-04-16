f0d9852506551cabb04357301a1c7126e4e15d23
Chiranjeev Gupta <chiranjeevgupta@AMGBLR3263-LT.local>
2023-04-16 20:57:40 +0530

    chore: update the start command to run node

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Other configuration options...
   generateBuildId: () => 'static',
-    assetPrefix: 'https://cdn.statically.io/gh/cran-cg/poc-2/main/',
+    assetPrefix: 'https://cdn.statically.io/gh/cran-cg/poc-2/abcb9ca1ff4154fcd122ff6175aad3f678bee34c/',
   };
   
